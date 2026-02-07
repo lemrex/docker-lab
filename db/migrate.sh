779047e6c0d98d917f63f3fdb2423a538477175a
steamcoder-droid <emmanuel.salau@yahho.com>
2026-02-07 09:54:40 +0000

    modify and add userid table

--- db/migrate.sh
+++ db/migrate.sh
@@ -5,7 +5,7 @@ echo "Running database migrations..."
 
 for f in /migrations/*.sql; do
   echo "Running $f"
-  psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "$f"
+  PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "$f"
 done
 
 echo "Migrations complete."
